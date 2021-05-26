7a4aa7b6c49b41eb1af7cecab00b09c2d1cc6520
Nat Dring <ndring@uvic.ca>
2021-05-25 21:25:56 -0700

    test graph made

--- foo.py
+++ foo.py
@@ -1,6 +1,7 @@
 import json
 import configparser
 import pandas as pd
+from graphviz import Graph
 
 config = configparser.ConfigParser()
 config.read('config/settings.ini')
@@ -10,5 +11,17 @@ interactions_data_file = config['DEFAULT']['data_dir_path'] + 'starwars-episode-
 with open(interactions_data_file) as f:
     interactions_data = json.load(f)
 
-character_df = pd.DataFrame(interactions_data['nodes'])
-link_df = pd.DataFrame(interactions_data['links'])
+# node_df = pd.DataFrame(interactions_data['nodes'])
+# link_df = pd.DataFrame(interactions_data['links'])
+
+dot = Graph(comment='SW EP1 INTERACTIONS')
+
+i=0
+for node in interactions_data['nodes']:
+    dot.node(str(i), label=node['name'], color=node['colour'])
+    i+=1
+
+for link in interactions_data['links']:
+    dot.edge(str(link['source']), str(link['target']), value=str(link['value']))
+
+dot.render('test-output/sw.gv', view=True)
