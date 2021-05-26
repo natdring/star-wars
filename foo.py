07a148783707edd6b795fc18e564303205f3102a
Nat Dring <ndring@uvic.ca>
2021-05-25 22:14:40 -0700

    using weighted graph

--- foo.py
+++ foo.py
@@ -2,11 +2,12 @@ import json
 import configparser
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 
 config = configparser.ConfigParser()
 config.read('config/settings.ini')
  
-interactions_data_file = config['DEFAULT']['data_dir_path'] + 'starwars-full-interactions.json'
+interactions_data_file = config['DEFAULT']['data_dir_path'] + 'starwars-episode-3-interactions.json'
 
 with open(interactions_data_file) as f:
     interactions_data = json.load(f)
@@ -14,13 +15,14 @@ with open(interactions_data_file) as f:
 # node_df = pd.DataFrame(interactions_data['nodes'])
 # link_df = pd.DataFrame(interactions_data['links'])
 
-G_symmetric = nx.Graph()
+G_weighted = nx.Graph()
 
 characters = []
 for node in interactions_data['nodes']:
     characters.append(node['name'])
 
 for link in interactions_data['links']:
-    G_symmetric.add_edge(characters[link['source']],characters[link['target']])
+    G_weighted.add_edge(characters[link['source']], characters[link['target']], weight=link['value'])
 
-nx.draw_networkx(G_symmetric)
+nx.draw_networkx(G_weighted)
+plt.show()
